3b67fdbfcb40a3de14509a165be9f7165c6cba64
GHhidatang <wai2333@163.com>
2021-02-01 10:49:49 +0800

    项目文件初始化

---
+++ cmake-build-debug/CMakeFiles/2_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_1.exe"
+  "2_1.exe.manifest"
+  "2_1.pdb"
+  "CMakeFiles/2_1.dir/main.c.obj"
+  "lib2_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
